37c3b2430dabf186381bb0be9344b5feb3b149ac
Leandro Rojano <leandrorojano2001@hotmail.com>
2024-07-07 21:49:05 -0300

    Agregado Vehículos

---
+++ js/vehicles.js
@@ -0,0 +1,86 @@
+const containercard = document.querySelector("#container-card");
+const pageDiv = document.querySelector("#page");
+const countDiv = document.querySelector("#count");
+let currentPage = 1;
+
+const obtenerVehicles = async (page) => {
+    try {
+        const response = await fetch(`https://swapi.dev/api/vehicles/?page=${page}`);
+        const data = await response.json();
+        createCards(data.results);
+        countDiv.textContent = `Vehículos encontrados: ${data.count}`;
+        pageDiv.textContent = `Página ${page}`; // Mostrar la página actual
+
+        // Habilitar o deshabilitar botones según la respuesta de la API
+        btnAnterior.disabled = !data.previous;
+        btnSiguiente.disabled = !data.next;
+    } catch (error) {
+        console.error("Error al obtener los Vehículos", error);
+    }
+};
+
+obtenerVehicles(currentPage);
+
+const createCards = async (vehicles) => {
+    containercard.innerHTML = ""; // Limpiar el contenedor antes de agregar las tarjetas
+
+    for (let vehicle of vehicles) {
+        const { name, model, manufacturer, cost_in_credits, length, crew, max_atmosphering_speed, passengers, cargo_capacity, consumables, starship_class} = vehicle;
+
+        containercard.innerHTML += `<div class="card">
+            <div class="card-body">
+                <h4 class="card-title">${name}</h4>
+                <br>
+                <p><span>Modelo:</span> ${model}</p>
+                <p><span>Fabricante:</span> ${manufacturer}</p>
+                <p><span>Precio:</span> ${cost_in_credits} créditos</p>
+                <p><span>Longitud:</span> ${length} m</p>
+                <p><span>Velocidad Máxima en atmósfera:</span> ${max_atmosphering_speed} km/h</p>
+                <p><span>Tripulación:</span> ${crew} tripulantes</p>
+                <p><span>Pasajeros:</span> ${passengers} pasajeros</p>
+                <p><span>Capacidad de carga:</span> ${cargo_capacity} tn/m2</p>
+                <p><span>Abastecimiento:</span> ${consumables}</p>
+                <p><span>Clase de Vehículo:</span> ${starship_class}</p>
+            </div>
+        </div>`;
+    }
+};
+
+function filterData(searchText) {
+    let cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        let cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
+        if (searchText == ' ' || searchText == '') {
+            card.style.display = 'block';
+
+        }
+        if (cardTitle.includes(searchText)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
+
+document.getElementById('searchInput').addEventListener('input', () => {
+    let searchText = document.getElementById('searchInput').value.trim().toLowerCase();
+
+    filterData(searchText);
+
+});
+
+// Agregar botones de navegación
+const btnAnterior = document.querySelector("#btn-anterior");
+const btnSiguiente = document.querySelector("#btn-siguiente");
+
+btnAnterior.addEventListener("click", () => {
+    if (currentPage > 1) {
+        currentPage--;
+        obtenerVehicles(currentPage);
+    }
+});
+
+btnSiguiente.addEventListener("click", () => {
+    currentPage++;
+    obtenerVehicles(currentPage);
+});
